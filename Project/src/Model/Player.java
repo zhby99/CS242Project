5429f3cb99e278d2847ca302dedf6e40c77a84cf
xdj701 <dajunxu2@illinois.edu>
2017-04-08 17:38:46 -0500

    noble and player

--- Project/src/Model/Player.java
+++ Project/src/Model/Player.java
@@ -4,4 +4,19 @@ package Model;
  * Created by wu on 4/8/17.
  */
 public class Player {
+	int id;
+	int score;
+	GemInfo gems;
+	int golds;
+	ArrayList<Card> cards;
+	ArrayList<Card> reserves;
+
+	Player(int pid){
+		id = pid;
+		score = 0;
+		gems = new GemInfo(0,0,0,0,0);
+		golds = 0;
+		cards = new ArrayList<Card>();
+		reserves = new ArrayList<Card>();
+	}
 }
