7f486d6a759bee19b0ba1ec89d55d3dda3a5761f
zhby99 <bzhang70@illinois.edu>
2017-04-08 17:36:16 -0500

    gem and card

--- Project/src/Model/Card.java
+++ Project/src/Model/Card.java
@@ -1,7 +1,33 @@
 package Model;
 
+import Model.utils.GemInfo;
+
 /**
  * Created by wu on 4/8/17.
  */
 public class Card {
+    final private int cardScore;
+    final private GemInfo developmentCost;
+    final private Gem targetGem;
+
+
+    public Card(int score, GemInfo developmentCost, Gem targetGem) {
+        this.cardScore = score;
+        this.developmentCost = developmentCost;
+        this.targetGem = targetGem;
+    }
+
+    public Gem getTargetGem(){
+        return targetGem;
+    }
+
+    public GemInfo getDevelopmentCost(){
+        return developmentCost;
+    }
+
+    public int getCardScore(){
+        return cardScore;
+    }
 }
+
+
--- Project/src/Model/Gem.java
+++ Project/src/Model/Gem.java
@@ -3,5 +3,36 @@ package Model;
 /**
  * Created by wu on 4/8/17.
  */
-public class Gem {
+public enum Gem {
+    DIAMOND("D",1){
+
+    },
+    EMERALD("E",2) {
+
+    },
+    ONYX("O", 3){
+
+    },
+    RUBY("R", 4){
+
+    },
+    SAPPHIRE("S", 5){
+
+    };
+
+    private String gemName;
+    private int index;
+
+    Gem(final String gemName, int index) {
+        this.gemName = gemName;
+        this.index = index;
+    }
+
+    public String getGemName(){
+        return gemName;
+    }
+    public int getIndex(){
+        return index;
+    }
+
 }
